f7179c43afbe73735f68d2c80f1d17bb36215056
Jessy Hu <jesshuy12@gmail.com>
2019-02-21 01:49:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,11 +20,11 @@ class Song
   end
   
   def self.artists
-    @@artists.uniq
+    @@artists.uniq    #unqie values only
   end
   
   def self.genres
-    @@genres.uniq 
+    @@genres.uniq     #unique values only
   end
   
   
