8a7ea125fe78ebfcb41e43b65b1320ab2bdffd85
Jessy Hu <jesshuy12@gmail.com>
2019-02-21 02:07:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,9 +30,12 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      genre_count[genre] += 1 
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count = 1
     end
-    puts genre_count
+    genre_count
   end
   
   
