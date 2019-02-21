6052e3e2f351bf6fe31a2b5da1350d9a1b1a9f7d
Jessy Hu <jesshuy12@gmail.com>
2019-02-21 02:22:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
   def self.genre_count
     genre_count = {}              #new empty hash
     @@genres.each do |genre|      #iterate thru @@genres
-      if genre_count[genre]       #if genre key value is in                                     genre_count hash, add 1
+      if genre_count[genre]       #if genre key value is present,                           increment 1
         genre_count[genre] += 1 
       else
         genre_count[genre] = 1
