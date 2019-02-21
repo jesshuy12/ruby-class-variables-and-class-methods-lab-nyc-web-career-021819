00db006b11ed3d75ff00b917415d6ad420ad3ca5
Jessy Hu <jesshuy12@gmail.com>
2019-02-21 01:39:14 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song 
+  
+  @@count = 0
+  @@genres = []
+  
+  attr_accessor :name, :artist, :genre
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+  end
+  
+  
+end
